05a3ce721d29fbcb435f0be62528fa2b87bb853f
Sardor Madaminov <sardor0968@gmail.com>
2023-12-16 16:38:03 +0900

    first commit

--- db/db_init.sql
+++ db/db_init.sql
@@ -4,11 +4,11 @@ VALUES(1, 'system', '', 'test@seoultech.ac.kr', TRUE, FALSE, TRUE, 1, NOW(), 1, 
 
 /*Admin user*/
 INSERT INTO account(account_id, username, password, email, first_name, last_name, active_status, deleted_status, immutable_status, creator, creation_date, updater, update_date)
-VALUES(2, 'admin', '$2a$10$Mz2N/DpFqDqZS85TSz4Lzussqkq3/siP6uulpcIfVH4VJuRLMjRtu', 'admin@gmail.com', 'Admin', 'Admin', TRUE, FALSE, TRUE, 1, NOW(), 1, NOW());
+VALUES(2, 'admin', '$hashcode for password', 'admin@gmail.com', 'Admin', 'Admin', TRUE, FALSE, TRUE, 1, NOW(), 1, NOW());
 
 /*General user - member*/
 INSERT INTO account(account_id, username, password, email, first_name, last_name, active_status, deleted_status, immutable_status, creator, creation_date, updater, update_date)
-VALUES(3, 'test', '$2a$10$Mz2N/DpFqDqZS85TSz4Lzussqkq3/siP6uulpcIfVH4VJuRLMjRtu', 'test@naver.com', 'Test', 'Test', TRUE, FALSE, TRUE, 1, NOW(), 1, NOW());
+VALUES(3, 'test', '$hashcode for password', 'test@naver.com', 'Test', 'Test', TRUE, FALSE, TRUE, 1, NOW(), 1, NOW());
 
 /*Roles for Spring Security*/
 INSERT INTO role(role_id, role_name, active_status, creation_date, creator, update_date, updater)
